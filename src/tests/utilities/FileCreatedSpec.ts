b6167726e4f9afb4bc9c65d13963760e699fb3ed
Charles <cobas050@uottawa.ca>
2021-07-25 05:10:54 -0400

    add test for fileExist function
    added a jasmine test for the fileExist function

---
+++ src/tests/utilities/FileCreatedSpec.ts
@@ -0,0 +1,19 @@
+import supertest from "supertest";
+import fileExist from "../../utilities/FileCreated";
+
+
+
+
+describe('Test for correct image paths', ()=>{
+    const imageExists = './assets/static/images/icelandwaterfall.jpg'; 
+    const imageNotExists = './assets/static/images/wate.jpg';
+    it('Tests if the path specified exists', async() =>{
+        const response = await fileExist(imageExists);
+        expect(response).toBeTruthy();
+    });
+    it('Tests for an incorrect path', async() =>{
+        const response = await fileExist(imageNotExists);
+        expect(response).toBeFalsy();
+    })
+
+})
